f476aed4328771859f0d28ff71300461b11cae89
Simon Long <simon@raspberrypi.com>
2020-10-16 14:59:30 +0100

    Add accessibility labels to localisation comboboxes

--- src/piwiz.c
+++ src/piwiz.c
@@ -1847,15 +1847,10 @@ int main (int argc, char *argv[])
     // set up the locale combo boxes
     read_locales ();
     wid = (GtkWidget *) gtk_builder_get_object (builder, "p1table");
-    country_cb = gtk_combo_box_new_with_model (GTK_TREE_MODEL (fcount));
-    language_cb = gtk_combo_box_new ();
-    timezone_cb = gtk_combo_box_new ();
-    gtk_table_attach (GTK_TABLE (wid), GTK_WIDGET (country_cb), 1, 2, 0, 1, GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 0, 0);
-    gtk_table_attach (GTK_TABLE (wid), GTK_WIDGET (language_cb), 1, 2, 1, 2, GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 0, 0);
-    gtk_table_attach (GTK_TABLE (wid), GTK_WIDGET (timezone_cb), 1, 2, 2, 3, GTK_FILL | GTK_SHRINK, GTK_FILL | GTK_SHRINK, 0, 0);
-    gtk_widget_set_tooltip_text (GTK_WIDGET (country_cb), _("Set the country in which you are using your Pi"));
-    gtk_widget_set_tooltip_text (GTK_WIDGET (language_cb), _("Set the language in which applications should appear"));
-    gtk_widget_set_tooltip_text (GTK_WIDGET (timezone_cb), _("Set the closest city to your location"));
+    country_cb = (GtkWidget *) gtk_builder_get_object (builder, "p1comb1");
+    language_cb = (GtkWidget *) gtk_builder_get_object (builder, "p1comb2");
+    timezone_cb = (GtkWidget *) gtk_builder_get_object (builder, "p1comb3");
+    gtk_combo_box_set_model (GTK_WIDGET (country_cb), GTK_TREE_MODEL (fcount));
 
     // set up cell renderers to associate list columns with combo boxes
     col = gtk_cell_renderer_text_new ();
