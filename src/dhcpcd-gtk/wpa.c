3cc820077958ac1545b695b39089086e1b1da221
Simon Long <simon@raspberrypi.com>
2021-01-27 12:07:38 +0000

    Remove use of deprecated stock strings

--- src/dhcpcd-gtk/wpa.c
+++ src/dhcpcd-gtk/wpa.c
@@ -126,8 +126,8 @@ wpa_configure(DHCPCD_WPA *wpa, DHCPCD_WI_SCAN *scan)
 	wpa_dialog = gtk_dialog_new_with_buttons(s.ssid,
 	    NULL,
 	    GTK_DIALOG_MODAL,
-	    GTK_STOCK_OK, GTK_RESPONSE_ACCEPT,
-	    GTK_STOCK_CANCEL, GTK_RESPONSE_REJECT,
+	    _("_OK"), GTK_RESPONSE_ACCEPT,
+	    _("_Cancel"), GTK_RESPONSE_REJECT,
 	    NULL);
 	gtk_window_set_position(GTK_WINDOW(wpa_dialog), GTK_WIN_POS_MOUSE);
 	gtk_window_set_resizable(GTK_WINDOW(wpa_dialog), false);
