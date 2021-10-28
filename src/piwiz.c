53d7b71477a67be789a26ffe34997b53b2b90f2a
Simon Long <simon@raspberrypi.com>
2021-10-28 12:00:27 +0100

    Add KMS HDMI audio addresses for Pi 1 / 2 / 3

--- src/piwiz.c
+++ src/piwiz.c
@@ -1909,6 +1909,8 @@ int main (int argc, char *argv[])
         if echo \"$SINKS\" | grep -q bcm2835_audio.digital-stereo; then OUTPUT=bcm2835_audio.digital-stereo ; \
         elif echo \"$SINKS\" | grep -q fef00700.hdmi.iec958-stereo ; then OUTPUT=fef00700.hdmi.iec958-stereo ; \
         elif echo \"$SINKS\" | grep -q fef05700.hdmi.iec958-stereo ; then OUTPUT=fef05700.hdmi.iec958-stereo ; \
+        elif echo \"$SINKS\" | grep -q 3f902000.hdmi.iec958-stereo ; then OUTPUT=3f902000.hdmi.iec958-stereo ; \
+        elif echo \"$SINKS\" | grep -q 20902000.hdmi.iec958-stereo ; then OUTPUT=20902000.hdmi.iec958-stereo ; \
         else OUTPUT=bcm2835_audio.analog-stereo ; fi ; \
         sudo -u $SUDO_USER XDG_RUNTIME_DIR=/run/user/$SUDO_UID pactl set-default-sink alsa_output.platform-$OUTPUT");
 
